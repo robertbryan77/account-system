c1d5579f650802aa95a91696d5496765333f5612
RobertBryan77 <robert.bryan3010@gmail.com>
2022-01-16 15:36:44 -0300

    add validation

--- src/app/Controllers/userController.js
+++ src/app/Controllers/userController.js
@@ -1,45 +1,87 @@
-const User = require('../models/User');
-const bcryptjs = require('bcryptjs');
+const User = require('../Models/User');
+const bcrypt = require('bcryptjs');
+const yup = require('yup');
 
-class userController {
+class UserController {
     show(req, res) {
-        console.log(req, res); {
-            var users = ["Robert", "Souza"]
-            return res.status(200).json({
-                error: false,
-                users
-            })
-        }
+        var users = ["Robert", "Souza"]
+        return res.status(200).json({
+            error: false,
+            users
+        })
     }
 
     async store(req, res) {
 
-        //Validação
+        /**
+         * Validação através do YUP schema
+         * Início
+         */
+        let schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required(),
+            password: yup.string().required()
+        });
 
-        const { name, email, password } = req.body;
-    
-        const data = {
-            name,
-            email,
-            password
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({
+                error: true,
+                message: "Dados inválidos"
+            })
         }
 
-        data.password = await bcryptjs.hash(data.password, 8)
+        /**
+        * Validação através do YUP schema
+        * fim
+        */
+
+        /**
+         * Validação no banco de dados
+         * Verifica se o usuário existe
+         */
+
+        let userExist = await User.findOne({ email: req.body.email });
+        if (userExist) {
+            return res.status(400).json({
+                error: true,
+                message: "Este usuário já existe!"
+            })
+        }
+
+        /**
+         * Desestrutuação dos dados da requisição
+         */
+        const { name, email, password } = req.body;
+
+        /**
+          * criação da constante data
+          */
+
+
+        const data = { name, email, password };
+
+        /**
+          * Criptografar a senha
+          */
+
+        data.password = await bcrypt.hash(data.password, 8);
+
+        /**
+          * Inserção no banco de dados 
+          */
 
         await User.create(data, (err) => {
-            if (err)
-                return res.statues(400).json({
-                    error: true,
-                    message: "Erro ao tentar inserir usuário no mongoDB"
-                })
+            if (err) return res.status(400).json({
+                error: true,
+                message: "Erro ao tentar inserir usuário no MongoDB"
+            })
 
             return res.status(200).json({
                 error: false,
-                message: "Usuário cadastrado com sucesso"
+                message: "Usuário Cadastrado com sucesso"
             })
         })
     }
-
 }
 
-module.exports = new userController();
+module.exports = new UserController();
