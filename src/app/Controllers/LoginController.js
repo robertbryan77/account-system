7b3b150ab9e7f15c052ef8adc6811917713dd67e
RobertBryan77 <robert.bryan3010@gmail.com>
2022-01-19 21:28:16 -0300

    refactor userExist

--- src/app/Controllers/LoginController.js
+++ src/app/Controllers/LoginController.js
@@ -1,26 +1,42 @@
 const User = require("../Models/User");
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../../config/auth')
 
 class LogiController {
 
     async index(req, res) {
         const { email, password } = req.body;
 
-        const userExist = await User.findOne({ email })
+        const userExist = await User.finOne({ email })
 
-        if(!userExist){
+        if (!userExist) {
             return res.status(400).json({
                 error: true,
                 message: "Usuário não existe!"
             })
         }
-        if(!(await bcryptjs.compare(userExist.password, userExist))) {
+
+        if (!(await bcryptjs.compare(userExist.password, userExist))) {
             return res.status(400).json({
                 error: true,
                 message: "A senha está inválida"
             })
         }
-       
+
+        // retorna o token do usuário
+
+        return res.status(200).json({
+            user: {
+                name: userExist.name,
+                email: userExist.email,
+            },
+            token: jwt.sign(
+                { id: userExist._id },
+                config.secret,
+                { expiresIn: config.expireIn })
+        })
+
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const { route } = require('express/lib/application');
+const LoginController = require('./app/Controllers/LoginController');
 // const { route } = require('./app');
 
 const userController = require('./app/Controllers/userController');
@@ -7,5 +9,7 @@ const routes = new Router();
 
 routes.post("/user", userController.store);
 routes.get("/user", userController.show);
+
+routes.get("/login", LoginController.index);
 
 module.exports = routes;
